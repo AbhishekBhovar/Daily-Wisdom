fc618f9471161343869031eae3276024773f939d
AbhishekBhovar <abhishekbhovar@gmail.com>
2026-09-05 11:47:40 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>event.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.registration.unregister();await self.clients.claim();})()));
-self.addEventListener('fetch',()=>{});
